90e2feff546d816497040651123ba7880ef2d5a5
Shabba <mc3dgar@gmail.com>
2023-08-11 23:40:47 +0100

    Added user into password generation

--- utils/password_generator.py
+++ utils/password_generator.py
@@ -4,8 +4,8 @@ import string
 import streamlit as st
 
 def generate_password(user):
-    characters = string.ascii_letters + string.digits
-    random.seed(st.secrets["auth"]["seed"])
+    characters = string.ascii_letters + string.digits + string.punctuation
+    random.seed(f'{st.secrets["auth"]["seed"]}{user}')
     password = ''.join(random.choice(characters) for _ in range(12))
 
     hashed = stauth.Hasher([password]).generate()[0]
