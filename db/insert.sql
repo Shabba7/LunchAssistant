ba3155150b5a01e0383b556bfc1b05943758b479
Nelson Gregorio <nelson.gregorio@live.com.pt>
2023-08-15 14:42:57 +0100

    Add Google API autocomplete for restaurants

--- db/insert.sql
+++ db/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO users (user_handle, user_name, pass_hash) VALUES('ngregori', 'Nelson
 INSERT INTO users (user_handle, user_name, pass_hash) VALUES('emoreira', 'Edgar Moreira', '$2b$12$UBTMLryvaFDpRQfuuaiOnuZYSs6WSHgK7I7uK6CyXy4S2aSCGkyB6');
 
 INSERT INTO restaurants (res_name, res_loc, res_user) VALUES('Passatempo', '-8.627761185482898, 41.156294713236775', '1');
-INSERT INTO restaurants (res_name, res_loc, res_user) VALUES('Mimo''s Smoke House ', '-8.632313323917877, 41.158335562806215', '2');
+INSERT INTO restaurants (res_name, res_loc, res_user) VALUES('Mimo''s Smoke House', '-8.632313323917877, 41.158335562806215', '2');
 INSERT INTO restaurants (res_name, res_loc, res_user) VALUES('Pizzaria Luzzo', '-8.633543714814978, 41.15845755163155', '1');
 INSERT INTO restaurants (res_name, res_loc, res_user) VALUES('San Remo', '-8.636919296674856, 41.15938796081641', '1');
 
--- webpages/suggestion_page.py
+++ webpages/suggestion_page.py
@@ -1,5 +1,21 @@
-import streamlit as st
 import db.db_handler as db
+import googlemaps
+import requests
+import streamlit as st
+from streamlit_searchbox import st_searchbox
+from typing import List
+import uuid
+
+
+@st.cache_resource()
+def init_gmaps():
+    return googlemaps.Client(key=st.secrets["google"]["api_key"], queries_per_second=1)
+
+@st.cache_data()
+def strypes_geo_coords():
+    return f'{st.secrets["strypes_geo"]["latitude"]},{st.secrets["strypes_geo"]["longitude"]}'
+
+SESSION_TOKEN = "3bd8f04e-8637-4fda-a686-d42a83dd076f"
 
 class Page:
 
@@ -8,16 +24,20 @@ class Page:
 
     def page(self):
 
-        with st.form("restaurant_picker", clear_on_submit=True):
-            st.subheader("New restaurant?")
-            restaurant = st.text_input(" ", placeholder="Type your restaurant suggestion here...",label_visibility="collapsed")
-            if st.form_submit_button("Submit"):
-                restaurants = [r.lower() for r in db.fetch_restaurants_names()]
-                if restaurant and restaurant.strip() != "" and restaurant.strip().lower() not in restaurants:
-                    db.register_restaurant(restaurant, '-8.62, 41.15', st.session_state['user_id'])
-                    st.info("Restaurant added! Thanks for contributing!", icon='😍')
-                elif restaurant and restaurant.strip() != "":
-                    st.info("Good pick! Someone already suggested one.", icon='😅')
+        st.subheader("New restaurant?")
+
+        restaurant = st_searchbox(self.restaurant_lookup, key="place_api_search")
+        st.write(restaurant)
+
+        # with st.form("restaurant_picker", clear_on_submit=True):
+        #     restaurant = st.text_input(" ", placeholder="Type your restaurant suggestion here...",label_visibility="collapsed")
+        #     if st.form_submit_button("Submit"):
+        #         restaurants = [r.lower() for r in db.fetch_restaurants_names()]
+        #         if restaurant and restaurant.strip() != "" and restaurant.strip().lower() not in restaurants:
+        #             db.register_restaurant(restaurant, '-8.62, 41.15', st.session_state['user_id'])
+        #             st.info("Restaurant added! Thanks for contributing!", icon='😍')
+        #         elif restaurant and restaurant.strip() != "":
+        #             st.info("Good pick! Someone already suggested it.", icon='😅')
 
         with st.form("suggestions", clear_on_submit=True):
             st.subheader("What can we improve?")
@@ -25,3 +45,17 @@ class Page:
             if st.form_submit_button("Submit"):
                 db.store_suggestion(st.session_state['user_id'][0],suggestion)
                 st.success("Thank you for your suggestion!")
+
+    @staticmethod
+    def restaurant_lookup(searchterm: str) -> List[str]:
+        #gmap = init_gmaps()
+        predictions = gmap.places_autocomplete(
+            input_text=searchterm, language="pt",
+            location=strypes_geo_coords(), radius=5000, strict_bounds=True,     # Geo  restrictions
+            components={"country":"PT"},                                        # Redundant?
+            origin=strypes_geo_coords(),                                        # For straight line distance calculation
+            types="food",                                                       # Type restrictions
+            session_token=SESSION_TOKEN                                         # Billing safety
+            # (Button to open session, generate token, close session with submit to db) # token per user? How streamlit does work?
+        )
+        return [f'{p["description"]} @ {p["distance_meters"]}m' for p in predictions]
