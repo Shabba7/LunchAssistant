9966be461d123551b7c87ecb36c027a5a7f5576b
Shabba <mc3dgar@gmail.com>
2023-08-19 00:51:26 +0100

    Made gmap page width instead of fixed size

--- webpages/picker_page.py
+++ webpages/picker_page.py
@@ -21,7 +21,7 @@ def build_map(choice):
         <iframe\
         resize: both;\
         overflow: auto;\
-        width="800" height="450"\
+        width="100%" height="450"\
         frameborder="0" style="border:0"\
         referrerpolicy="no-referrer-when-downgrade"\
         src="{map_url}"\
